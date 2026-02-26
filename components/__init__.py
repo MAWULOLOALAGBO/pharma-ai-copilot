0701eed270df15979e3d6f001c83ad3ef51d4937
Mawulolo ALAGBO <parfaitalagbo18@gmail.com>
2026-02-26 10:08:26 +0100

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,1 +1,7 @@
+"""
+Components package for Pharma-AI Copilot.
+"""
 
+from .visualizations import AutoVizGenerator, render_visualizations, suggest_insights
+
+__all__ = ['AutoVizGenerator', 'render_visualizations', 'suggest_insights']
