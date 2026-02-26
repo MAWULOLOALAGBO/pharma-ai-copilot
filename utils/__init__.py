817ef539e86b2d7abf607cc7ccac3205a3e16a87
Mawulolo ALAGBO <parfaitalagbo18@gmail.com>
2026-02-26 16:33:11 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,4 +4,4 @@ Utils package for Pharma-AI Copilot.
 
 from .schema_detector import ColumnDetector, quick_detect, format_detection_results
 
-__all__ = ['ColumnDetector', 'quick_detect', 'format_detection_results']
+__all__ = ['ColumnDetector', 'quick_detect', 'format_detection_results','ExcelReportGenerator', 'generate_excel_report']
