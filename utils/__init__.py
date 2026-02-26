c820c3ca1b242c180fdc64036b13d410f05fc9c7
Mawulolo ALAGBO <parfaitalagbo18@gmail.com>
2026-02-26 09:36:24 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,7 @@
+"""
+Utils package for Pharma-AI Copilot.
+"""
 
+from .schema_detector import ColumnDetector, quick_detect, format_detection_results
+
+__all__ = ['ColumnDetector', 'quick_detect', 'format_detection_results']
